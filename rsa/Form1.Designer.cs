c4862b14ecf728497d295fe4433b21d7457e8800
lucas <mrmqs159@gmail.com>
2020-03-02 12:05:09 +0100

    program

---
+++ rsa/Form1.Designer.cs
@@ -0,0 +1,194 @@
+﻿namespace rsa
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.textBox5 = new System.Windows.Forms.TextBox();
+            this.textBox6 = new System.Windows.Forms.TextBox();
+            this.button3 = new System.Windows.Forms.Button();
+            this.textBox7 = new System.Windows.Forms.TextBox();
+            this.radioButton1 = new System.Windows.Forms.RadioButton();
+            this.radioButton2 = new System.Windows.Forms.RadioButton();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(12, 12);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(142, 20);
+            this.textBox1.TabIndex = 0;
+            this.textBox1.Text = "p";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 38);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(142, 46);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Generuj klucze";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(161, 12);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(142, 20);
+            this.textBox2.TabIndex = 2;
+            this.textBox2.Text = "q";
+            this.textBox2.TextChanged += new System.EventHandler(this.textBox2_TextChanged);
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(161, 64);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.ReadOnly = true;
+            this.textBox3.Size = new System.Drawing.Size(142, 20);
+            this.textBox3.TabIndex = 3;
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(161, 38);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.ReadOnly = true;
+            this.textBox4.Size = new System.Drawing.Size(142, 20);
+            this.textBox4.TabIndex = 4;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(12, 162);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(291, 23);
+            this.button2.TabIndex = 5;
+            this.button2.Text = "szyfruj";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // textBox5
+            // 
+            this.textBox5.Location = new System.Drawing.Point(12, 191);
+            this.textBox5.Name = "textBox5";
+            this.textBox5.Size = new System.Drawing.Size(291, 20);
+            this.textBox5.TabIndex = 6;
+            // 
+            // textBox6
+            // 
+            this.textBox6.Location = new System.Drawing.Point(12, 105);
+            this.textBox6.Name = "textBox6";
+            this.textBox6.Size = new System.Drawing.Size(291, 20);
+            this.textBox6.TabIndex = 7;
+            this.textBox6.TextChanged += new System.EventHandler(this.textBox6_TextChanged);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(12, 217);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(291, 23);
+            this.button3.TabIndex = 8;
+            this.button3.Text = "odszyfruj";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // textBox7
+            // 
+            this.textBox7.Location = new System.Drawing.Point(12, 246);
+            this.textBox7.Name = "textBox7";
+            this.textBox7.Size = new System.Drawing.Size(291, 20);
+            this.textBox7.TabIndex = 9;
+            // 
+            // radioButton1
+            // 
+            this.radioButton1.AutoSize = true;
+            this.radioButton1.Location = new System.Drawing.Point(43, 131);
+            this.radioButton1.Name = "radioButton1";
+            this.radioButton1.Size = new System.Drawing.Size(86, 17);
+            this.radioButton1.TabIndex = 10;
+            this.radioButton1.TabStop = true;
+            this.radioButton1.Text = "Szyfruj liczbe";
+            this.radioButton1.UseVisualStyleBackColor = true;
+            this.radioButton1.CheckedChanged += new System.EventHandler(this.radioButton1_CheckedChanged);
+            // 
+            // radioButton2
+            // 
+            this.radioButton2.AutoSize = true;
+            this.radioButton2.Location = new System.Drawing.Point(184, 131);
+            this.radioButton2.Name = "radioButton2";
+            this.radioButton2.Size = new System.Drawing.Size(80, 17);
+            this.radioButton2.TabIndex = 11;
+            this.radioButton2.TabStop = true;
+            this.radioButton2.Text = "szyfruj tekst";
+            this.radioButton2.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(325, 277);
+            this.Controls.Add(this.radioButton2);
+            this.Controls.Add(this.radioButton1);
+            this.Controls.Add(this.textBox7);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.textBox6);
+            this.Controls.Add(this.textBox5);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.TextBox textBox5;
+        private System.Windows.Forms.TextBox textBox6;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.TextBox textBox7;
+        private System.Windows.Forms.RadioButton radioButton1;
+        private System.Windows.Forms.RadioButton radioButton2;
+    }
+}
+
---
+++ rsa/Form1.cs
@@ -0,0 +1,223 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Numerics;
+
+namespace rsa
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int p = Convert.ToInt32(textBox1.Text);
+            int q = Convert.ToInt32(textBox2.Text);
+            int eukval = 0;
+            int oeukval = 0;
+            int n = p * q;
+            int eee = (p-1) * (q-1);
+            for (int i = 3; i < eee; i += 2) {
+                if (gcf(i, eee) == 1)
+                {
+                    eukval = i;
+                    break;
+                }
+            }
+            for (int i = 2; i < eee; i += 1)
+            {
+                if (ogcf(i, eukval,eee) == 1)
+                {
+                    oeukval = i;
+                    break;
+                }
+            }
+
+            textBox3.Text = eukval.ToString() + "," + n.ToString();
+            textBox4.Text = oeukval.ToString() + "," + n.ToString();
+
+        }
+
+
+        static int gcf(int a, int b)
+        {
+            while (b != 0)
+            {
+                int temp = b;
+                b = a % b;
+                a = temp;
+            }
+            return a;
+        }
+        static int ogcf(int a, int b,int c)
+        {
+            if ((a * b) % c == 1)
+            {
+                return 1;
+            }
+            return 0;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            char[] litery;
+            string publiczny = textBox3.Text;
+            string[] k = publiczny.Split(',');
+            int[] publiczne = { Convert.ToInt32(k[0]), Convert.ToInt32(k[1]) };
+            if (radioButton1.Checked)
+            {
+                BigInteger bigintiger;
+                BigInteger dos = BigInteger.Parse(textBox6.Text);
+                bigintiger = dos;
+                for (int bi = 0; bi < publiczne[0] - 1; bi += 1)
+                {
+                    bigintiger = bigintiger * dos;
+                }
+
+                bigintiger = bigintiger % publiczne[1];
+                textBox5.Text = bigintiger.ToString();
+            }else if (radioButton2.Checked)
+            {
+                string s = "";
+                byte[] ASCIIValues = Encoding.ASCII.GetBytes(textBox6.Text.ToUpper());
+                foreach (byte b in ASCIIValues)
+                {
+                    //MessageBox.Show(Convert.ToInt32(b).ToString());
+                    BigInteger bigintiger=0;
+                    BigInteger dos= Convert.ToInt32(b);
+                    bigintiger = dos;
+                    for (int bi = 0; bi < publiczne[0] - 1; bi += 1)
+                    {
+                        //MessageBox.Show(Convert.ToInt32(b).ToString());
+                        bigintiger = bigintiger * dos;
+                    }
+
+                    bigintiger = bigintiger % publiczne[1];
+                    s += ","+bigintiger.ToString();
+                    
+                    //MessageBox.Show(Convert.ToInt32(b).ToString());
+                    textBox5.Text = s.ToString();
+
+
+                }
+
+
+            }
+            
+           
+        }
+
+        private void textBox6_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            string prywatne = textBox4.Text;
+            string[] k = prywatne.Split(',');
+            int[] priv = { Convert.ToInt32(k[0]), Convert.ToInt32(k[1]) };
+           
+            if (radioButton1.Checked)
+            {
+                Dictionary<int, double> potegi2 = new Dictionary<int, double>();
+                double dor = Convert.ToDouble(textBox5.Text);
+                potegi2.Add(1, dor % priv[1]);
+                do
+                {
+                    potegi2.Add(potegi2.Last().Key * 2, Math.Pow(potegi2.Last().Value, 2) % priv[1]);
+                } while (potegi2.Last().Key < priv[0] / 2);
+
+
+                BigInteger sum = 1;
+                while (priv[0] != 0)
+                {
+                    if (priv[0] >= potegi2.Last().Key)
+                    {
+                        sum *= Convert.ToInt64(potegi2.Last().Value);
+                        priv[0] -= potegi2.Last().Key;
+                        //MessageBox.Show(Convert.ToString(sum));
+                    }
+                    else
+                    {
+                        potegi2.Remove(potegi2.Last().Key);
+                    }
+                }
+                sum = sum % priv[1];
+                textBox7.Text = sum.ToString();
+            }
+            else if (radioButton2.Checked)
+            {
+                string wyraz="";
+                string[] sar;
+                sar= textBox5.Text.Split(',');
+                var slist=sar.ToList();
+                slist.RemoveAt(0);
+                sar = slist.ToArray();
+                string s = sar.ToString();
+                int temppriv0 = priv[0];
+                foreach (string str in sar)
+                {
+                    priv[0] = temppriv0;
+                    double strval = Convert.ToDouble(str);
+                    Dictionary<int, double> potegi2 = new Dictionary<int, double>();
+                    
+                    potegi2.Add(1, strval % priv[1]);
+                    do
+                    {
+                        potegi2.Add(potegi2.Last().Key * 2, Math.Pow(potegi2.Last().Value, 2) % priv[1]);
+                    } while (potegi2.Last().Key < priv[0] / 2);
+
+
+                    BigInteger sum = 1;
+                    while (priv[0] != 0)
+                    {
+                        if (priv[0] >= potegi2.Last().Key)
+                        {
+                            sum *= Convert.ToInt64(potegi2.Last().Value);
+                            priv[0] -= potegi2.Last().Key;
+                            //MessageBox.Show(Convert.ToString(sum));
+                        }
+                        else
+                        {
+                            potegi2.Remove(potegi2.Last().Key);
+                        }
+                    }
+                    sum = sum % priv[1];
+                    char c = (char)sum;
+                    //textBox7.Text = c.ToString();
+                    wyraz += c.ToString();
+
+                }
+
+                textBox7.Text = wyraz;
+
+            }
+            
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void radioButton1_CheckedChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
